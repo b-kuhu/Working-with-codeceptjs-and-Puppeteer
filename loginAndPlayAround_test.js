2c3efb696b72557fc7fc85a2f811469bc6345665
b-kuhu <Kuhu Bhatnagar>
2022-09-21 17:37:33 +0530

    user action on a page

--- loginAndPlayAround_test.js
+++ loginAndPlayAround_test.js
@@ -2,4 +2,10 @@ Feature('login');
 
 Scenario('test something', ({ I }) => {
 I.amOnPage('/index.html');
+I.fillField("Name","kuhu");
+I.fillField("Email","123@gmail.com");
+I.selectOption('Role','Admin');
+I.checkOption('Accept');
+I.click("//input[@type='login");
+
 });
